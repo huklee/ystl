2b58e27e425694e4e27eb45cb327920eab257307
huklee <lhwnotss@gmail.com>
2017-01-22 10:10:06 +0900

    added iterator interfaces

--- src/h_uk.lee/ystl.cpp
+++ src/h_uk.lee/ystl.cpp
@@ -1,9 +1,11 @@
 // interface
 
+// the basic container
 template<class T>
 class node{
 private:
 	T val;
+	node* prev;
 	node* next;
 public:
 	node();
@@ -13,14 +15,13 @@ public:
 template<class T>
 class list{
 private:
-	node<T>* head;		
+	node<T>* head;
 public:
 	list();
 	list(const T &val);
 };
 
 // implementation
-
 template<class T>
 node<T>::node(){};
 
@@ -28,8 +29,8 @@ template<class T>
 node<T>::node(const T& val){
 	this->val = val;
 	this->next = NULL;
+	this->prev = NULL;
 }
-
 
 template<class T>
 list<T>::list(){};
@@ -37,4 +38,27 @@ list<T>::list(){};
 template<class T>
 list<T>::list(const T &val){
 	this->head = new node<T>(val);
+}
+
+// iterator (=smart pointer)
+template<typename T>
+class list_iterator{
+	node<T>* cur;
+public:
+	list_iterator(node<T>* p=0): cur(p) {}
+	inline list_iterator& operator++();
+	inline list_iterator& operator--();
+	inline bool list_iterator& operator==();
+	inline bool list_iterator& operator!=();
+}
+
+// alias for iterator
+typedef list_iterator<T> iterator;
+
+iterator begin(){
+	return iterator(head);
+}
+
+iterator end(){
+	return iterator(0);
 }
